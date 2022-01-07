9c94f8589d6219bbf32a37defb97a3f4ce4279ee
pmozola <mozola.pawel@gmail.com>
2022-01-07 15:33:11 +0100

    fix name

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blogApp'),
+      dir: require('path').join(__dirname, './coverage/pmozola-blog'),
       subdir: '.',
       reporters: [
         { type: 'html' },
